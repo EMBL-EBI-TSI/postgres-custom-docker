b837d8b51df4c7ec43a600289c779a05e0ddc254
Amélie Cornélis <ameliec@ebi.ac.uk>
2017-09-12 10:59:14 +0100

    Extract tests to a script
    This allows to re-use them in my pre-push git hook, to force
    running of the tests
    Also, we're now only adding the image in the local repo on success,
    preventing mis-configurations to contaminate downstream.

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -o errexit
+
+DOCKER_IMAGE=postgres-custom-ci
+PG_PORT=5438
+PG_USER=build
+PG_PWD=buildsecret
+
+docker build -t $DOCKER_IMAGE .
+DOCKER_INSTANCE=`docker run -d --rm -p $PG_PORT:5432 -e POSTGRES_USER=$PG_USER -e POSTGRES_PASSWORD=$PG_PWD $DOCKER_IMAGE`
+echo Started $DOCKER_INSTANCE
+sleep 5s
+PGPASSWORD=$PG_PWD psql -h localhost -p $PG_PORT -U $PG_USER postgres -c 'select count(*) from pg_stat_activity;'
+echo Stopping
+docker stop $DOCKER_INSTANCE
