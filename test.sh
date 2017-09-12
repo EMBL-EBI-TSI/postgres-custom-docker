62aa221fa53c0b2cb64cd9fd2b193632ba88c58c
Amélie Cornélis <ameliec@ebi.ac.uk>
2017-09-12 15:13:28 +0000

    Adapt to performance profile of the jenkins box
    5s worked plenty on my machine, but does not seem to be enough when
    running on jenkins. I did look into using something more sofisticated
    but there does not seem to be a 'actually ready' event that docker
    itself can emit: most people are waiting on a port being open or the
    likes, which is not really helping here.

--- test.sh
+++ test.sh
@@ -18,5 +18,5 @@ PG_PWD=buildsecret
 docker build -t $DOCKER_IMAGE .
 DOCKER_INSTANCE=`docker run -d --rm -p $PG_PORT:5432 -e POSTGRES_USER=$PG_USER -e POSTGRES_PASSWORD=$PG_PWD $DOCKER_IMAGE`
 echo Started $DOCKER_INSTANCE
-sleep 5s
+sleep 10s
 PGPASSWORD=$PG_PWD psql -h localhost -p $PG_PORT -U $PG_USER postgres -c 'select count(*) from pg_stat_activity;'
