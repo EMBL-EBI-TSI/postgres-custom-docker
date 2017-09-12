b7cd3c003e62f0a1d93a446516b0e5f467cac684
Amélie Cornélis <ameliec@ebi.ac.uk>
2017-09-12 11:46:30 +0100

    Better error handling: stop container on test fail
    Use the bash equivalent of try/finally to, i.e. trap <tidy> EXIT, to
    detect whether we had successfully started a container (by looking
    in the variable) and if so, stop it.
    
    This way, the test (psql command) is still the last call, so the
    script as a whole still returns a failure when the test fail.

--- test.sh
+++ test.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-set -o errexit
+function finally {
+  if [ -n "$DOCKER_INSTANCE" ]
+  then
+    echo Stopping $DOCKER_INSTANCE
+    docker stop $DOCKER_INSTANCE
+  fi
+}
+
+trap finally EXIT
 
 DOCKER_IMAGE=postgres-custom-ci
 PG_PORT=5438
@@ -12,5 +20,3 @@ DOCKER_INSTANCE=`docker run -d --rm -p $PG_PORT:5432 -e POSTGRES_USER=$PG_USER -
 echo Started $DOCKER_INSTANCE
 sleep 5s
 PGPASSWORD=$PG_PWD psql -h localhost -p $PG_PORT -U $PG_USER postgres -c 'select count(*) from pg_stat_activity;'
-echo Stopping
-docker stop $DOCKER_INSTANCE
