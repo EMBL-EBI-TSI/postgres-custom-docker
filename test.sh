3d05f5cc232b7bf85d53f667a3044d5b36d003d4
Amélie Cornélis <ameliec@ebi.ac.uk>
2018-02-13 09:59:57 +0000

    Fail test if container does not start healthily
    Make sure this script returns a non-zero code when the container
    fails to start with a 'healthy' state.

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 function finally {
   if [ -n "$DOCKER_INSTANCE" ]
@@ -15,11 +16,18 @@ PG_PORT=5438
 PG_USER=build
 PG_PWD=buildsecret
 
+# Start the container
 docker build -t $DOCKER_IMAGE .
 DOCKER_INSTANCE=`docker run -d --rm -p $PG_PORT:5432 -e POSTGRES_USER=$PG_USER -e POSTGRES_PASSWORD=$PG_PWD $DOCKER_IMAGE`
 echo Started $DOCKER_INSTANCE
+
+# wait until we know whether the things inside are alive/dead
 until docker inspect --format "{{index .State.Health.Status}}" $DOCKER_INSTANCE | grep -q healthy; do echo -n "."; sleep 1; done
+DOCKER_STATE=$(docker inspect --format "{{index .State.Health.Status}}" $DOCKER_INSTANCE)
 echo
-echo Healthy $DOCKER_INSTANCE
+echo "State $DOCKER_STATE"
+if [ "$DOCKER_STATE" = "unhealthy" ]; then exit 1; fi
+
+# now perform the test
 echo Starting tests
 PGPASSWORD=$PG_PWD psql -h localhost -p $PG_PORT -U $PG_USER postgres -c 'select count(*) from pg_stat_activity;'
