176f29edf4640774d2c03fd7baf1e88ff4a7cd37
Amélie Cornélis <ameliec@ebi.ac.uk>
2018-02-13 10:21:51 +0000

    Reflect actual use case more closely
    We don't use the default 'postgres' database when running the
    container, so make sure the test reflect that.

--- test.sh
+++ test.sh
@@ -15,10 +15,11 @@ DOCKER_IMAGE=postgres-custom-ci
 PG_PORT=5438
 PG_USER=build
 PG_PWD=buildsecret
+PG_DB_NAME=builddb
 
 # Start the container
 docker build -t $DOCKER_IMAGE .
-DOCKER_INSTANCE=$(docker run -d --rm -p $PG_PORT:5432 -e POSTGRES_USER=$PG_USER -e POSTGRES_PASSWORD=$PG_PWD $DOCKER_IMAGE)
+DOCKER_INSTANCE=$(docker run -d --rm -p $PG_PORT:5432 -e POSTGRES_USER=$PG_USER -e POSTGRES_PASSWORD=$PG_PWD -e POSTGRES_DB=$PG_DB_NAME $DOCKER_IMAGE)
 echo Started $DOCKER_INSTANCE
 
 # wait until we know whether the things inside are alive/dead
@@ -30,4 +31,4 @@ if [ "$DOCKER_STATE" = "unhealthy" ]; then exit 1; fi
 
 # now perform the test
 echo Starting tests
-PGPASSWORD=$PG_PWD psql -h localhost -p $PG_PORT -U $PG_USER postgres -c 'select count(*) from pg_stat_activity;'
+PGPASSWORD=$PG_PWD psql -h localhost -p $PG_PORT -U $PG_USER $PG_DB_NAME -c 'select count(*) from pg_stat_activity;'
